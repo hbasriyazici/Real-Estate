b63f21292d6a6a35603d1dd556d30bee271ae5dd
bilalsevis <bilalresmi@gmail.com>
2020-01-27 16:28:03 +0300

    Updates

---
+++ precache-manifest.ad74789dd036e447d7c27b3b7eb19113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb8c64019a99cdcb5c8100a4838fd3d",
+    "url": "/Real-Estate/index.html"
+  },
+  {
+    "revision": "9f9d2c6805d3ffaeec43",
+    "url": "/Real-Estate/static/css/main.21226521.chunk.css"
+  },
+  {
+    "revision": "edfb8c0d53e2863fa097",
+    "url": "/Real-Estate/static/js/2.c93ff5fd.chunk.js"
+  },
+  {
+    "revision": "80f887ecd8e7f2b705b21a0a76fe0e53",
+    "url": "/Real-Estate/static/js/2.c93ff5fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f9d2c6805d3ffaeec43",
+    "url": "/Real-Estate/static/js/main.bf9b4077.chunk.js"
+  },
+  {
+    "revision": "409379f0dd7ba76bcca0",
+    "url": "/Real-Estate/static/js/runtime-main.90215f2c.js"
+  }
+]);
