21f9d9e44f73e519c6b95640ac87497377504381
Bilal Sevinc <bilalresmi@gmail.com>
2020-03-17 19:25:31 +0300

    bug fixes

--- src/App.js
+++ src/App.js
@@ -18,27 +18,26 @@ import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 
-export const  store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends Component {
   render() {
     return (
-        <Provider store={store}>
-            <HashRouter basename="/">
-            <React.Fragment>
+      <Provider store={store}>
+        <HashRouter basename="/">
+          <React.Fragment>
             <ScrollToTop/>
-                <Switch>
-                    <Route exact path={'/'} component={Home}/>
-                    <Route exact path={'/rent'} component={Rent}/>
-                    <Route exact path={'/tour'} component={Tour}/>
-                    <Route exact path={'/school-registration'} component={School}/>
-                    <Route exact path={'/contact'} component={Contact}/>
-                    <Route exact path={'/rent/:id'} component={ProductDetail}/>
-                </Switch>
-                {/* <Footer/> */}
-            </React.Fragment>
-            </HashRouter>
-        </Provider>
+            <Switch>
+              <Route exact path={'/'} component={Home}/>
+              <Route exact path={'/rent'} component={Rent}/>
+              <Route exact path={'/tour'} component={Tour}/>
+              <Route exact path={'/school-registration'} component={School}/>
+              <Route exact path={'/contact'} component={Contact}/>
+              <Route exact path={'/rent/:id'} component={ProductDetail}/>
+            </Switch>
+          </React.Fragment>
+        </HashRouter>
+      </Provider>
     );
   }
 }
